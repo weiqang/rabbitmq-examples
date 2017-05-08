69249f89a65026610537f636115e09590a203955
pakhomov <nikita.pakhomov@exquance.com>
2017-05-09 00:07:50 +0300

    Add PubSub example

---
+++ src/PubSub/ReceiveLogs/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "192.168.0.101" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                    var queueName = channel.QueueDeclare().QueueName;
+                    channel.QueueBind(queue: queueName,
+                        exchange: "logs",
+                        routingKey: "");
+
+                    Console.WriteLine(" [*] Waiting for logs.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: queueName,
+                        noAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
